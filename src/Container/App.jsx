c7af687f2038123c487acd3d4138a0b1462a1d16
Thyago Rafael <thyagorafael012@gmail.com>
2021-03-02 15:20:27 -0300

    Import fix

--- src/Container/App.jsx
+++ src/Container/App.jsx
@@ -1,9 +1,8 @@
 import React from 'react';
 import { ApolloProvider } from '@apollo/react-hooks';
 import { getClient } from '../services/rickAndMortyGraphQLApi';
-import './App.css';
-
 import AppData from './AppData';
+import './App.css';
 
 export default function App() {
 	return <ApolloProvider client={getClient()} children={<AppData />} />;
