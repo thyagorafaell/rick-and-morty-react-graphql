83b66baac9da9104e9a7a3dfda9aee0f378d9f22
Thyago Rafael <thyagorafael012@gmail.com>
2021-03-03 01:46:41 -0300

    Convert Section to styled component

--- src/Components/Generic/Section/Section.jsx
+++ src/Components/Generic/Section/Section.jsx
@@ -1,14 +1,31 @@
 import React, { memo } from 'react';
-import './Section.css';
+import styled from 'styled-components';
+
+const Container = styled.section`
+    width: 100%;
+`;
+
+const Title = styled.h4`
+    color: var(--yellow);
+    font-family: Nunito;
+    font-size: 13px;
+    letter-spacing: 0.65em;
+    line-height: 18px;
+    text-transform: uppercase;
+`;
+
+const Content = styled.div`
+    margin-top: 9px;
+`
 
 function Section({ className, children, title }) {
     return (
-        <div className={`w-full ${className ? className : ''}`}>
-            { title && <h4 className={'section-title text-yellow'}>{title}</h4> }
-            <div className={'section-data'}>
+        <Container className={className}>
+            { title && <Title>{title}</Title> }
+            <Content>
                 { children }
-            </div>
-        </div>
+            </Content>
+        </Container>
     )
 }
 
