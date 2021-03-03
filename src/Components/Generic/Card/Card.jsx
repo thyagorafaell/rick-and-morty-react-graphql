2b69981b181bc58cc67b27be664f4ad8afde20df
Thyago Rafael <thyagorafael012@gmail.com>
2021-03-03 00:12:45 -0300

    Convert card to styled component (partially)

--- src/Components/Generic/Card/Card.jsx
+++ src/Components/Generic/Card/Card.jsx
@@ -1,5 +1,46 @@
 import React, { memo } from 'react';
+import styled from 'styled-components';
 import './Card.css';
+
+const Base = styled.div`
+    border: 2px solid var(--gray);
+    border-radius: 8px;
+    overflow: hidden;
+
+    :hover, :active {
+        border: 2px solid var(--yellow);
+        box-shadow: 0px 6px 7px rgba(203, 215, 54, 0.25);
+    }
+`;
+
+const Background = styled.div`
+    width: 100%;
+    height: 100%;
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-position: top;
+`;
+
+const Data = styled.div`
+    background: rgba(30, 30, 30, 0.8);
+    backdrop-filter: var(--blur);
+    bottom: 0;
+    color: var(--white);
+    min-height: 55px;
+    padding: 10px;
+    position: sticky;
+`;
+
+const Title = styled.p`
+    font-size: 20px;
+    font-weight: 700;
+    line-height: 25px;
+`;
+
+const Subtitle = styled.p`
+    font-size: 12px;
+    line-height: 15px;
+`;
 
 function Card({ className = '', disabled, image, title, subtitle, width, height }) {
     const stateClass = disabled ? 'card--disabled' : 'card--enabled';
@@ -14,15 +55,15 @@ function Card({ className = '', disabled, image, title, subtitle, width, height 
     };
 
     return (
-        <div className={`card card-action overflow-hidden ${className} ${stateClass}`} style={cardStyleFromProps}>
-            <div className={'card-background bg-cover bg-no-repeat bg-top h-full w-full'} style={cardBackgroundStyleFromProps}></div>
+        <Base className={`card-action ${className} ${stateClass}`} style={cardStyleFromProps}>
+            <Background className={'card-background'} style={cardBackgroundStyleFromProps} />
             { showData && (
-                <div className={'card-data sticky'}>
-                    { title && <p className={'card-title font-bold'}>{title}</p> }
-                    { subtitle && <p className={'card-subtitle'}>{subtitle}</p> }
-                </div>
+                <Data>
+                    { title && <Title>{title}</Title> }
+                    { subtitle && <Subtitle>{subtitle}</Subtitle> }
+                </Data>
             )}
-        </div>
+        </Base>
     );
 }
 
