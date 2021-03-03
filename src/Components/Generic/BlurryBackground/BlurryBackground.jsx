127eb4374619a923e3ce86a44bbec7dafdb5d382
Thyago Rafael <thyagorafael012@gmail.com>
2021-03-02 23:55:19 -0300

    Convert BlurryBackground to styled component

--- src/Components/Generic/BlurryBackground/BlurryBackground.jsx
+++ src/Components/Generic/BlurryBackground/BlurryBackground.jsx
@@ -1,15 +1,39 @@
 import { createPortal } from 'react-dom';
 import React from 'react';
-import './BlurryBackground.css';
+import styled from 'styled-components';
+
+const Container = styled.div`
+    height: 100vh;
+    width: 100vw;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    align-items: center;
+    justify-content: center;
+    display: flex;
+    position: fixed;
+    z-index: 50;
+`;
+
+const Blurry = styled.div`
+    height: 100vh;
+    width: 100vw;
+    backdrop-filter: var(--blur);
+`;
+
+const Content = styled.div`
+    position: absolute;
+`;
 
 export default function BlurryBackground({ children }) {
     const loading = (
-        <div className={'h-screen w-screen fixed inset-0 z-50 flex items-center justify-center'}>
-            <div className={'h-screen w-screen blurry-background'}></div>
-            <div className={'absolute'}>
+        <Container>
+            <Blurry />
+            <Content>
                 { children }
-            </div>
-        </div>
+            </Content>
+        </Container>
     );
 
     return createPortal(loading, document.body);
